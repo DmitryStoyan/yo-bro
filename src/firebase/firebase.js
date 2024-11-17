2cf594c3a97aa6853978e638f81142b30b183ef6
DmitryStoyan <jombee@yandex.ru>
2024-11-17 20:00:35 +0300

    first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4GvD95cepNp3WDqpnSmUIx_vspSAma-8",
+  authDomain: "yo-bro-1d8af.firebaseapp.com",
+  projectId: "yo-bro-1d8af",
+  storageBucket: "yo-bro-1d8af.firebasestorage.app",
+  messagingSenderId: "783564485342",
+  appId: "1:783564485342:web:d8db20218c261876d6884f",
+};
+
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <Registration />
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+
+import Registration from './Registration.vue'
+</script>
