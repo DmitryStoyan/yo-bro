540080c59dd3b3cb7127acdc6de7c8fcf13c80d5
DmitryStoyan <jombee@yandex.ru>
2025-03-24 12:44:05 +0300

    fix: Refresh incoming requests after loading userId

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -12,13 +12,19 @@ onMounted(() => {
   userStore.getFriendRequests()
 })
 
+watchEffect(() => {
+  if (userStore.userId) {
+    userStore.getFriendRequests();
+  }
+});
+
 </script>
 
 <template>
   <div>
     <h2>Уведомления</h2>
-    <!-- <q-btn color="primary" icon="check" label="OK" @click="userStore.getFriendRequests" /> -->
-    <ul>
+    <p v-if="userStore.incomingRequests.length == 0">Нет уведомлений</p>
+    <ul v-else>
       <li v-for="request in userStore.incomingRequests" :key="request.id">
         {{ request.fromUserName }}
       </li>
