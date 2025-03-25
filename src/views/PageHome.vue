7e6377eca5e22cddc9eae91ec1fdca63ba9070a4
DmitryStoyan <jombee@yandex.ru>
2025-03-25 21:15:06 +0300

    style: added styles to PageHome

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -3,14 +3,25 @@
 
     <q-img class="bg-img" src="../assets/background/homeBG.gif" />
 
-    <h1>Главная страница</h1>
-    <p>Это приложение..... Тут будет описание</p>
-    <q-btn label="Регистрация/Вход" @click="goToAuth" v-if="!userStore.userId" />
-    <q-btn label="Выход" @click="logOut" v-else />
-    <q-btn label="Профиль" @click="goToProfile" />
+    <header class="header">
+      <img class="logo" src="../assets/logo/logo.jpg" alt="Yo">
+      <q-btn class="auth-btn" label="Регистрация/Вход" @click="goToAuth" v-if="!userStore.userId" />
+      <q-btn class="auth-btn" label="Выход" @click="logOut" v-else />
+    </header>
+
+    <div class="description-wrapper">
+      <h1 class="title">Yo Bro</h1>
+      <p class="subtitle">Простой способ сказать Yo Bro!</p>
+      <p class="description">
+        Yo Bro — это самое быстрое и лаконичное приложение для общения! Всего одно нажатие — и твои друзья получают
+        дружеское "Bro". Без лишних слов, без сложных интерфейсов, только мгновенная связь и фановый вайб.
+      </p>
+
+      <p class="description">Присоединяйся и начни общаться проще, чем когда-либо! 🚀</p>
+    </div>
+    <!-- <q-btn label="Профиль" @click="goToProfile" />
     <q-btn label="Друзья" @click="goToFriendsList" />
-    <q-btn label="Уведомления" @click="goToNotification" />
-    <checkAuth />
+    <q-btn label="Уведомления" @click="goToNotification" /> -->
   </q-page>
 </template>
 
@@ -47,6 +58,7 @@ const logOut = async () => {
 
 <style scoped>
 .page {
+  color: #fff;
   position: relative;
   height: 100vh;
 }
@@ -59,5 +71,51 @@ const logOut = async () => {
   left: 0;
   object-fit: cover;
   z-index: -1;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px 30px 0 30px;
+  margin: 0 0 100px 0;
+}
+
+.logo {
+  width: 30px;
+  height: 30px;
+}
+
+.auth-btn {
+  outline: 1px #fff solid !important;
+}
+
+.description-wrapper {
+  max-width: 1200px;
+  width: 80%;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.title {
+  font-size: 100px;
+  font-weight: 300;
+  margin: 0 0 15px 0;
+}
+
+.subtitle {
+  font-size: 24px;
+  font-weight: 300;
+  margin: 0 0 50px 0;
+}
+
+.description {
+  font-size: 20px;
+  font-weight: 400;
+  margin: 0 0 60px 0;
+}
+
+.description:last-of-type {
+  margin: 0;
 }
 </style>
